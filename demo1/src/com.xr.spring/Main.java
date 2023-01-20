cfba40957b8f0fdf9b5b109476fc72e88244cfc2
buptzt <ztrhzt@163.com>
2023-01-20 15:18:39 +0800

    '第二次'

--- demo1/src/com.xr.spring/Main.java
+++ demo1/src/com.xr.spring/Main.java
@@ -1,11 +1,21 @@
 package com.xr.spring.controller;
 
+import com.xr.spring.dao.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+    @Autowired
+    User user;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        new Main().test();
+    }
+
+    public void test(){
+        System.out.println(user.getName());
     }
 }
